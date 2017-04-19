1241296ee470ad1aab0745b9b2498a3150662c7d
storost@web.de <storost@web.de>
2017-04-19 23:55:02 +0200

    init

--- ProgramGUI.cs
+++ ProgramGUI.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -37,6 +38,10 @@ namespace ConsoleStartsForm
 
     class ProgramGUI
     {
+
+        [DllImport("kernel32.dll", EntryPoint = "GetConsoleWindow")]
+        private static extern IntPtr _GetConsoleWindow();
+
         //[DllImport("kernel32.dll")]
         //static extern bool FreeConsole();
 
@@ -58,7 +63,18 @@ namespace ConsoleStartsForm
             IntPtr ptr = NativeMethods.GetForegroundWindow();
             int u;
             NativeMethods.GetWindowThreadProcessId(ptr, out u);
+            //
+
             Process process = Process.GetProcessById(u);
+
+
+            //var childPidToParentPid = GetAllProcessParentPids();
+            //int currentProcessId = Process.GetCurrentProcess().Id;
+
+            //Console.WriteLine("Current Process ID: " + currentProcessId);
+            //Console.WriteLine("Parent Process ID: " + childPidToParentPid[currentProcessId]);
+            //MessageBox.Show(childPidToParentPid[currentProcessId].ToString());
+
 
             if (args.Length == 0)
             {
@@ -69,37 +85,68 @@ namespace ConsoleStartsForm
             }
             else
             {
-                // Command line given, display console
-                //if (!AttachConsole(-1)) { 
-                //    // Attach to a parent process console
-                //    AllocConsole(); // Alloc a new console if none available
-                //    ConsoleMain(args);
-                //    attached = true;
-                //}
-
-                if (string.Compare(process.ProcessName, "cmd", StringComparison.InvariantCultureIgnoreCase) == 0)
+                var consoleHandle = _GetConsoleWindow();
+                // run GUI
+                //if (consoleHandle == IntPtr.Zero || AppDomain.CurrentDomain.FriendlyName.Contains(".vshost")) {
+                if (AppDomain.CurrentDomain.FriendlyName.Contains(".vshost"))
                 {
-                    // attach to the current active console
-                    NativeMethods.AttachConsole(process.Id);
-                    attached = true;
+                MessageBox.Show("Visual Studio Call or consoleHandle? ");
+                    // we either have no console window or we're started from within visual studio
+                    // This is the form I usually run. Change it to match your code.
+                    Application.Run(new Main());
                 }
                 else
                 {
-                    // create new console
-                    NativeMethods.AllocConsole();
+
+                    // command line given, display console
+                    //if (!NativeMethods.AttachConsole(-1))
+                    //{
+                    //    // attach to a parent process console
+                    //    NativeMethods.AllocConsole(); // alloc a new console if none available
+                    //    ConsoleMain(args);
+                    //    attached = true;
+                    //}
+
+                    if (string.Compare(process.ProcessName, "cmd", StringComparison.InvariantCultureIgnoreCase) == 0)
+                    {
+                        // attach to the current active console
+                        //MessageBox.Show("attach" + process.Id.ToString());
+                        NativeMethods.AttachConsole(process.Id);
+                        attached = true;
+                        //Console.Write("your output");
+                    }
+                    else
+                    {
+                        // create new console
+                        NativeMethods.AllocConsole();
+                    }
+
+                    //Console.Write("your output");
+                    ConsoleMain(args);
+
+
+                    NativeMethods.FreeConsole();
+                    if (attached)
+                    {
+                        var hwnd = process.MainWindowHandle;
+                        NativeMethods.PostMessage(hwnd, NativeMethods.WM_KEYDOWN, NativeMethods.VK_RETURN, 0);
+                       
+                    }
                 }
 
-                Console.Write("your output");
-                ConsoleMain(args);
+                //Console.WriteLine("console found");
+                //// we found a console attached to us, so restart ourselves without one
+                //Process.Start(new ProcessStartInfo(Assembly.GetEntryAssembly().Location)
+                //{
+                //    CreateNoWindow = true,
+                //    UseShellExecute = false
+                //});
+                //MessageBox.Show("Else");
+            
 
-
-                NativeMethods.FreeConsole();
-                if (attached)
-                {
-                    var hWnd = process.MainWindowHandle;
-                    NativeMethods.PostMessage(hWnd, NativeMethods.WM_KEYDOWN, NativeMethods.VK_RETURN, 0);
-                }
             }
+
+                
 
         }
         
@@ -130,6 +177,55 @@ namespace ConsoleStartsForm
 
             //Console.ReadKey();
             //string s = Console.ReadLine();
+        }
+
+        public static Dictionary<int, int> GetAllProcessParentPids()
+        {
+            var childPidToParentPid = new Dictionary<int, int>();
+
+            var processCounters = new SortedDictionary<string, PerformanceCounter[]>();
+            var category = new PerformanceCounterCategory("Process");
+
+            // As the base system always has more than one process running, 
+            // don't special case a single instance return.
+            var instanceNames = category.GetInstanceNames();
+            foreach (string t in instanceNames)
+            {
+                try
+                {
+                    processCounters[t] = category.GetCounters(t);
+                }
+                catch (InvalidOperationException)
+                {
+                    // Transient processes may no longer exist between 
+                    // GetInstanceNames and when the counters are queried.
+                }
+            }
+
+            foreach (var kvp in processCounters)
+            {
+                int childPid = -1;
+                int parentPid = -1;
+
+                foreach (var counter in kvp.Value)
+                {
+                    if ("ID Process".CompareTo(counter.CounterName) == 0)
+                    {
+                        childPid = (int)(counter.NextValue());
+                    }
+                    else if ("Creating Process ID".CompareTo(counter.CounterName) == 0)
+                    {
+                        parentPid = (int)(counter.NextValue());
+                    }
+                }
+
+                if (childPid != -1 && parentPid != -1)
+                {
+                    childPidToParentPid[childPid] = parentPid;
+                }
+            }
+
+            return childPidToParentPid;
         }
     }
 }
